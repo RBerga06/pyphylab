54dbb17248b4a2018d37e4bb61d128ff84a448e6
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-11-18 23:44:23 +0100

    Added Measure & Datum

---
+++ src/rberga06/phylab/measure.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Abstract measure."""
+from dataclasses import dataclass
+from typing import Protocol, Self, final
+
+
+class _Measure(Protocol):
+    best: float
+    delta: float
+
+    @property
+    def delta_rel(self, /) -> float:
+        return self.delta / self.best
+
+    # --- Unary operators ---
+
+    def __pos__(self, /) -> Self:
+        return self
+
+    def __neg__(self, /) -> "_Measure":
+        return Datum(-self.best, self.delta)
+
+    # --- Binary operators ---
+
+    def __add__(self, other: "_Measure | float") -> "_Measure":
+        if isinstance(other, float | int):
+            return Datum(self.best + other, self.delta)
+        return Datum(self.best + other.best, self.delta + other.delta)
+
+    def __sub__(self, other: "Measure | float", /) -> "Measure":
+        return self + (-other)
+
+    def __mul__(self, other: "Measure | float", /) -> "Measure":
+        if isinstance(other, float | int):
+            return Datum(self.best * other, self.delta * abs(other))
+        return Datum.from_delta_rel(self.best * other.best, self.delta_rel + other.delta_rel)
+
+    def __truediv__(self, other: "Measure | float", /) -> "Measure":
+        if isinstance(other, float | int):
+            return Datum(self.best / other, self.delta / abs(other))
+        return Datum.from_delta_rel(self.best / other.best, self.delta_rel + other.delta_rel)
+
+    def __pow__(self, other: int, /) -> "Measure":
+        return Datum.from_delta_rel(self.best ** other, self.delta_rel * abs(other))
+
+    # --- Right operands ---
+
+    def __radd__(self, other: "Measure | float", /) -> "Measure":
+        return self + other
+
+    def __rsub__(self, other: "Measure | float", /) -> "Measure":
+        return (-self) + other
+
+    def __rmul__(self, other: "Measure | float", /) -> "Measure":
+        return self * other
+
+    def __rtruediv__(self, other: "Measure | float", /) -> "Measure":
+        if isinstance(other, float | int):
+            return Datum.from_delta_rel(other / self.best, self.delta_rel)
+        return Datum.from_delta_rel(other.best / self.best, other.delta_rel + self.delta_rel)
+
+
+class Measure(_Measure, Protocol):
+    @property
+    def best(self, /) -> float: ...  # type: ignore[reportIncompatibleOverride]
+    @property
+    def delta(self, /) -> float: ...  # type: ignore[reportIncompatibleOverride]
+
+
+@final
+@dataclass(slots=True, frozen=True)
+class Datum(_Measure):
+    best: float
+    delta: float
+
+    @classmethod
+    def from_const(cls, const: float, /) -> Self:
+        return cls(const, 0)
+
+    @classmethod
+    def from_delta_rel(cls, best: float, delta_rel: float, /) -> Self:
+        return cls(best, delta_rel * best)
+
+
+__all__ = ["Measure", "Datum"]
