a56749562670e193c2802c3cbca811016ced73ef
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-11-19 11:02:30 +0100

    Added abstract distributions

---
+++ src/rberga06/phylab/distribution.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from dataclasses import dataclass
+from typing import Iterable, Iterator, Protocol, Sequence, overload
+from .measure import Measure
+from .range import Range
+
+
+class _ImmutableDataProxy[T](Protocol):
+    """A proxy around the `data` attribute."""
+    data: Sequence[T]
+
+    def __len__(self, /) -> int:
+        return len(self.data)
+
+    def __iter__(self, /) -> Iterator[T]:
+        return iter(self.data)
+
+    @overload
+    def __getitem__(self, key: int, /) -> T: ...
+    @overload
+    def __getitem__(self, key: slice, /) -> Sequence[T]: ...
+    def __getitem__(self, key: int | slice, /) -> T | Sequence[T]:
+        return self.data[key]
+
+
+
+class AbstractDist(Protocol):
+    def probability(self, x: Range, /) -> float: ...
+
+
+@dataclass(slots=True, frozen=True)
+class DistBin[M: Measure]:
+    """A dynamic container for distribution bins."""
+    _dist: "Dist[M]"
+    r: Range
+
+    @property
+    def center(self, /) -> float:
+        return (self.r.right + self.r.left)/2
+
+    @property
+    def data(self, /) -> tuple[M, ...]:
+        return tuple([d for d in self._dist.data if d.best in self.r])
+
+
+class Dist[M: Measure](_ImmutableDataProxy[M], Measure, AbstractDist, Protocol):
+    """An (abstract) distribution."""
+    data: Sequence[M]
+
+    @property
+    def binsr(self, /) -> Iterable[Range]: ...
+
+    @property
+    def bins(self, /) -> tuple[DistBin[M], ...]:
+        return tuple([DistBin(self, r) for r in self.binsr])
+
+    @overload
+    def expected(self, x: None = ..., /) -> float: ...
+    @overload
+    def expected(self, x: Range, /) -> float: ...
+    def expected(self, x: Range | None = None, /) -> float | tuple[float, ...]:
+        if x is None:
+            n = len(self.data)
+            return tuple([self.probability(r)*n for r in self.binsr])
+        return self.probability(x) * len(self.data)
