7e8c7bb3be59fe98f00321f3684842896d27e025
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-11-23 15:55:57 +0100

    More tests for range

--- tests/test_range.py
+++ tests/test_range.py
@@ -10,5 +10,22 @@ class TestRange:
         assert r == r[:] == r[:12] == r[-1:]
         assert R[-1:.2] == Range("[", -1, .2, "]")
 
-    def test_ops(self, /) -> None:
-        assert NotImplemented
+    def test_split(self, /) -> None:
+        assert R[0:5].split(5) == (
+            Range("[", 0, 1, "]"),
+            Range("(", 1, 2, "]"),
+            Range("(", 2, 3, "]"),
+            Range("(", 3, 4, "]"),
+            Range("(", 4, 5, "]"),
+        )
+
+    def test_contains(self, /) -> None:
+        assert 0 not in R[1:]
+        assert 1 not in R[:-1]
+        assert .5 in R[0:]
+        assert .5 in R[:1]
+        assert 567438 in R
+        assert 0 not in R["(0;+1]"]
+        assert 0 not in R["[-1;0)"]
+        assert 0 in R["[0;+1)"]
+        assert 0 in R["(-1;0]"]
