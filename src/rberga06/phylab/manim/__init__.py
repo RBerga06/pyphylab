5cea726dcd5fa55e867d5108ac7af38f734be365
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-11-25 15:16:41 +0100

    Add __all__

--- src/rberga06/phylab/manim/__init__.py
+++ src/rberga06/phylab/manim/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # pyright: reportMissingTypeStubs=false
+# ruff: noqa: F403,
 """Manim CE support/utilities."""
-from .hist import DiscreteDistributionHistogram
+from .hist import *
