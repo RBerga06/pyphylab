1d593fbfc22dbf6f2b2242a846cfee2d4333fbae
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-11-26 19:45:45 +0100

    Add ability to set start & stop. Also, fix bar labels.

--- src/rberga06/phylab/manim/hist.py
+++ src/rberga06/phylab/manim/hist.py
@@ -4,7 +4,7 @@
 # pyright: reportUnknownMemberType=false
 # pyright: reportIncompatibleVariableOverride=false
 """Histogram for Distributions."""
-from typing import Any, Sequence
+from typing import Any, Literal, Sequence
 from manim import BarChart, Circle
 from manim.typing import Point3D
 from manim.constants import MED_SMALL_BUFF, DEFAULT_DOT_RADIUS
@@ -37,16 +37,30 @@ class DiscreteDistributionHistogram[D: DiscreteDist[Any]](BarChart):
         self,
         dist: D,
         /, *,
+        x_start: int | None = None,
+        x_stop:  int | None = None,
         bar_names: Sequence[str] | None = None,
         bar_colors: Sequence[ManimColor | str] = DEFAULT_BAR_COLORS,
+        bar_labels: Sequence[str] | Literal["auto"] | None = "auto",
         **kwargs: Any,
     ) -> None:
         self.dist = dist
-        bins: list[float] = [len(b) for b in dist.bins]
+        dbins: list[tuple[int, float]] = [(int(b.center), len(b)) for b in dist.bins]
+        if dbins:
+            if x_start is not None:
+                dbins = [(i, 0) for i in range(x_start, dbins[0][0] + 1)] + dbins
+            if x_stop is not None:
+                dbins += [(i, 0) for i in range(dbins[-1][0], x_stop + 1)]
+        else:
+            if x_start is not None and x_stop is not None:
+                dbins = [(i, 0) for i in range(x_start, x_stop + 1)]
+        if bar_labels == "auto":
+            bar_labels = [str(b[0]) for b in dbins]
         super().__init__(
-            bins,
+            [b[1] for b in dbins],
             bar_names=bar_names,
             bar_colors=bar_colors,  # type: ignore
+            bar_labels=bar_labels,
             **kwargs,
         )
 
