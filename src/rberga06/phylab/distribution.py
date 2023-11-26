3f3bce0309aad2c3f80608e4a73537a087f67d6b
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-11-26 22:17:26 +0100

    Add custom_bins_start, custom_bins_stop

--- src/rberga06/phylab/distribution.py
+++ src/rberga06/phylab/distribution.py
@@ -3,7 +3,7 @@
 from dataclasses import dataclass
 from math import ceil, floor
 from typing import Iterable, Iterator, Protocol, Self, Sequence, final, overload, override
-from .measure import Measure, AnyMeasureLike, MeasureLike
+from .measure import Measure, MeasureLike
 from .range import Range
 
 
@@ -21,7 +21,7 @@ def best[X: (float, int)](x: MeasureLike[X], /) -> X:
     return x.best
 
 
-class _DataSequence[M: AnyMeasureLike](Protocol):
+class _DataSequence[M: MeasureLike[float]](Protocol):
     """A proxy around the `data` attribute."""
     data: Sequence[M]
 
@@ -37,7 +37,7 @@ class _DataSequence[M: AnyMeasureLike](Protocol):
 
 @final
 @dataclass(slots=True, frozen=True)
-class DistBin[M: AnyMeasureLike](_DataSequence[M]):
+class DistBin[M: MeasureLike[float]](_DataSequence[M]):
     """A dynamic container for distribution bins."""
     dist: "Dist[M]"
     r: Range
@@ -53,9 +53,23 @@ class DistBin[M: AnyMeasureLike](_DataSequence[M]):
 
 
 
-class Dist[M: AnyMeasureLike](_DataSequence[M], Measure[float], Protocol):
+class Dist[M: MeasureLike[float]](_DataSequence[M], Measure[float], Protocol):
     """An (abstract) distribution."""
     data: Sequence[M]
+    custom_bins_start: float | None = None
+    custom_bins_stop:  float | None = None
+
+    @property
+    def bins_start(self, /) -> float:
+        if self.custom_bins_start is None:
+            return min(map(best, self.data))
+        return self.custom_bins_start
+
+    @property
+    def bins_stop(self, /) -> float:
+        if self.custom_bins_stop is None:
+            return min(map(best, self.data))
+        return self.custom_bins_stop
 
     @property
     def binsr(self, /) -> Iterable[Range]: ...
