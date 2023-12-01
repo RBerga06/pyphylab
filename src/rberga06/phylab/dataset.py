3df8200bf16a24f8e54d38922ae6ac29ea316052
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-12-01 08:58:34 +0100

    Fix tests

--- src/rberga06/phylab/dataset.py
+++ src/rberga06/phylab/dataset.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """A concrete data set."""
 from dataclasses import dataclass
-from typing import Callable, Sequence, final, override
+from typing import TYPE_CHECKING, Callable, Sequence, final, override
 
 from .bins import ADataSet, BinSet
 from .measure import MeasureLike
@@ -13,15 +13,16 @@ from .measure import MeasureLike
 class DataSet[X: MeasureLike[float]](ADataSet[X]):
     data: Sequence[X]  # pyright: ignore[reportIncompatibleMethodOverride]
 
-    # We have to re-define this because we don't have HKTs.
-    @override
-    def intbins[T: MeasureLike[int]](self: "DataSet[T]", /) -> BinSet[T, "DataSet[T]"]:
-        return super().intbins()  # type: ignore
+    if TYPE_CHECKING:
+        # We have to re-define these methods because we don't have HKTs.
 
-    # We have to re-define this because we don't have HKTs.
-    @override
-    def map[A: MeasureLike[float], B: MeasureLike[float]](self: "DataSet[A]", f: Callable[[A], B], /) -> "DataSet[B]":
-        return super().map(f)  # type: ignore
+        @override
+        def intbins[T: MeasureLike[int]](self: "DataSet[T]", /) -> BinSet[T, "DataSet[T]"]:
+            return super().intbins()  # type: ignore
+
+        @override
+        def map[A: MeasureLike[float], B: MeasureLike[float]](self: "DataSet[A]", f: Callable[[A], B], /) -> "DataSet[B]":
+            return super().map(f)  # type: ignore
 
 
 __all__ = ["DataSet"]
